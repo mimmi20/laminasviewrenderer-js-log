b1789a1db494417e509dc17d7c7a746065c99e40
Thomas Müller <mimmi20@live.de>
2023-11-18 16:51:22 +0100

    update js

--- template/logger.js
+++ template/logger.js
@@ -27,37 +27,55 @@ function logErrorData(errorData) {
 
       return response.blob();
     })
-    .catch(function(error) {
+    .catch(error => {
       console.error(error);
     });
 }
 
-window.addEventListener('error', error => {
-  let errorData = {
-    message: error.message,
-    type: error.type.toString(),
-    filename: error.filename ?? null,
+window.addEventListener('error', event => {
+  const errorData = {
+    message: event.message,
+    type: event.type.toString(),
+    filename: event.filename ?? null,
     element: null,
     url: window.location.href
   };
 
   // If target is HTML Element save it
-  if (error.target.nodeName) {
-    errorData.element = error.target.outerHTML;
+  if (event.target.nodeName) {
+    errorData.element = event.target.outerHTML;
   }
 
   // Check if error is Missing SOURCE
-  if (error.target.nodeName === 'IMG' || error.target.nodeName === 'SOURCE' || error.target.nodeName === 'IFRAME' || error.target.nodeName === 'VIDEO') {
+  if (event.target.nodeName === 'IMG' || event.target.nodeName === 'SOURCE' || event.target.nodeName === 'IFRAME' || event.target.nodeName === 'VIDEO') {
     errorData.message = 'Invalid or missing source';
   }
 
   logErrorData(errorData);
-}, true);
+  event.preventDefault();
+});
 
 window.addEventListener('unhandledrejection', event => {
-  let errorData = {
+  const errorData = {
     message: event.reason.stack.toString(),
-    type: event.reason.name ?? "Promise error",
+    type: event.reason.name ?? 'Promise error',
+    filename: null,
+    element: null,
+    url: window.location.href
+  };
+
+  if (event.reason?.response.status) {
+    errorData.message = `Status: ${event.reason?.response.status} ${event.reason?.response.url} ${event.reason.stack.toString()}`
+  }
+
+  logErrorData(errorData);
+  event.preventDefault();
+});
+
+window.addEventListener('messageerror', event => {
+  const errorData = {
+    message: event.toString(),
+    type: 'Message error',
     filename: null,
     element: null,
     url: window.location.href
